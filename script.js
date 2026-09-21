b269732d8ba700245d87987416cfa8872a12e8e0
Simone <133378830+codiz7@users.noreply.github.com>
2026-09-21 02:15:43 +0200

    Update script.js
    modificato la scritta per il disclaimer dei cookie, aggiunto chiarimento sull'utilizzo di questi ultimi

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function renderGate() {
   embedContainer.innerHTML = `
     <div class="cookie-gate">
       <p class="cookie-gate__text">
-        Per ascoltare i brani devi accettare i cookie di terze parti impostati da SoundCloud.
+        Per ascoltare i brani è necessario accettare i cookie di terze parti impostati da SoundCloud necessari al funzionamento del player.
         <a href="privacy.html">Scopri di più</a>.
       </p>
       <div class="cookie-gate__actions">
